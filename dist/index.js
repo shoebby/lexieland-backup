16e1176e2111d8a05222169f8872f5d2bd396b36
shoebby <alexandria.vaneekelen@gmail.com>
2025-03-20 23:46:35 +0100

    DESTROYED last.fm widget

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,3 @@
-//import "../node_modules/jquery/dist/jquery.js";
-
 //how tf do i not make it need to do this bru
 window.onload = () => {
     //background music
@@ -22,34 +20,6 @@ window.onload = () => {
     // function isPlaying(audioEl) {
     //     return !audioEl.paused;
     // }
-
-    //checking last.fm for most recent track
-    var link = document.getElementById("trackLink");
-    var cover = document.getElementById("trackCover");
-    var title = document.getElementById("trackName");
-    var artist = document.getElementById("trackArtist");
-
-    // function CurrentlyPlaying() {
-    //     //eslint-disable-next-line no-undef
-    //     $.get(`http://ws.audioscrobbler.com/2.0/`,
-    //         {
-    //             "method": "user.getrecenttracks",
-    //             "limit": "1",
-    //             "user": "shoebby",
-    //             "api_key": API_KEY,
-    //             "format": "json"
-    //         }, (data, status) => {
-    //             console.log(data);
-    //             console.log(status);
-
-    //             cover.src = data.recenttracks.track[0].image[1]["#text"];
-    //             link.href = data.recenttracks.track[0].url;
-    //             title.innerHTML = data.recenttracks.track[0].name;
-    //             artist.innerHTML = data.recenttracks.track[0].artist["#text"];
-    //         }
-    //     )
-    // };
-    // CurrentlyPlaying();
 
     //pfp arf and shake on click
     var pfp = document.getElementById("pfp");
