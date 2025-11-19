59a08734e7c7b358bf69c2723139b08b52cf5ce8
Alexandria <alexandria.vaneekelen@gmail.com>
2025-11-19 17:24:38 +0100

    first look at assessment....

--- dist/assessment/assessment.js
+++ dist/assessment/assessment.js
@@ -1,10 +1,14 @@
-let windowObjectReference = null; // global variable
+let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+
+console.log("width: " + vw + "| height: " + vh);
+
 const features_sier = "left=775,top=600,width=500,height=420";
-const features_spr = "left=1325,top=600,width=500,height=420";
+const features_spr = "left=1325,top=600,width=750,height=750";
 
 function openRequestedTab() {
-    var sier = window.open('https://www.lexie.land/dist/sierpinski.html', '1', features_sier);
-    var spr = window.open('https://www.lexie.land/dist/sprout_div.html', '2', features_spr);
+    let spr = window.open('https://www.lexie.land/dist/visualizer.html', '1', features_spr);
+    let sier = window.open('https://www.lexie.land/dist/sierpinski.html', '2', features_sier);
 }
 
 const linker = document.querySelector("input[target='OpenWindows']");
