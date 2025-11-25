7c949b37197e2189d4ec2040f09e9f0c4aba6a35
Alexandria <alexandria.vaneekelen@gmail.com>
2025-11-25 13:53:45 +0100

    maximazing, restoring, and closing windows + start on start menu

--- dist/mytoys.js
+++ dist/mytoys.js
@@ -16,12 +16,40 @@ document.querySelectorAll('table.interactive').forEach(element => {
 
 $( function() {
     $( ".container" ).draggable({
+        handle: ".title-bar",
         stack: ".ui-draggable", /* Stack the currently dragged item on top of all other items. */
 		distance: 0, /* I believe this has to do with mouse distance? */
 		containment: "document", /* Makes it so pieces don't get lost off the page while dragging them */
     });
 } );
 
-function doThing() {
-    console.log("boyvagina");
+document.querySelectorAll("button[target='maximize']").forEach(element => {
+    element.addEventListener('click', (event) => {
+        ToggleWindowSize(element);
+    })
+});
+document.querySelectorAll("button[target='close']").forEach(element => {
+    element.addEventListener('click', (event) => {
+        element.parentNode.parentNode.parentNode.parentNode.open = false;
+    })
+});
+
+function ToggleWindowSize(button) {
+    const target = button.parentNode.parentNode.parentNode;
+
+    if (button.getAttribute("aria-label") == 'Maximize') {
+        target.setAttribute('style', 'width: 100vw; height: 100vh; position: fixed; top: 0; left: 0;');
+        button.setAttribute('aria-label','Restore');
+    }
+    else if (button.getAttribute("aria-label") == 'Restore') {
+        const daddy = target.parentNode.id;
+        if (daddy == 'cat_fractal' || daddy == 'cat_popup' || daddy == 'cat_art'){
+            target.setAttribute('style', 'width: 36vw;');
+        } else if (daddy == 'cat_media') {
+            target.setAttribute('style', 'width: 25vw;');
+        } else if (daddy == 'cat_recurse') {
+            target.setAttribute('style', 'width: 90vw;');
+        }
+        button.setAttribute('aria-label','Maximize');
+    }
 }
